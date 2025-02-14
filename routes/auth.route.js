12da5be24d47c5de0752a9f6d47d210aa71ef037
Wasifalikhan064 <wasifalikhan064@gmail.com>
2025-02-14 07:46:30 +0530

    bugs fixed

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { validateLogin, validateSignup } from "../utils/validator.js";
 import { login, resetPassword, signup } from "../controllers/auth.controller.js";
-
+import { verifyToken } from "../middleware/verifyToken.js";
 const router= express.Router()
 
 router.post("/signup",validateSignup,signup)
 router.post("/login",validateLogin,login)
-router.post("/reset-password",resetPassword); 
+router.post("/reset-password",verifyToken,resetPassword); 
 
 export default router;
