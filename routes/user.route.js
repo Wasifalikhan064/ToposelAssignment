9ae45872f7a0ef275a1c1e4b167254f781ffaa59
Wasifalikhan064 <wasifalikhan064@gmail.com>
2025-02-14 07:43:08 +0530

    fixed

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,10 +1,9 @@
 import express from "express";
 import { searchUser } from "../controllers/auth.controller.js";
-import { verifyToken } from "../middleware/verifyToken.js";
 import { getAllUsers } from "../controllers/user.controller.js";
 
 const router= express.Router()
 router.get("/",getAllUsers);
-router.get("/search",verifyToken,searchUser)
+router.get("/search",searchUser)
 
 export default router;
